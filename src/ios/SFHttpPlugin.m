f41cd0f24cb0f7c0d529f8d722f2fa638fb81a88
Simão Ferreira <spaf94@outlook.pt>
2018-06-09 08:50:44 -0700

    Methods setBaseAddress and setTimeouts for iOS

--- src/ios/SFHttpPlugin.m
+++ src/ios/SFHttpPlugin.m
@@ -26,11 +26,16 @@ NSInteger *readTimeout = 0;
 - (void)setTimeouts:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* pluginResult = nil;
-    NSString* base_Address = [command.arguments objectAtIndex:0];
+    NSString* str_connect_timeout = [command.arguments objectAtIndex:0];
+    NSString* str_read_timeout = [command.arguments objectAtIndex:1];
+    NSInteger connect_timeout = [str_connect_timeout intValue];
+    NSInteger read_timeout = [str_read_timeout intValue];
 
-    if (base_Address != nil && [base_Address length] > 0) {
-    	baseAddress = base_Address;
-        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:baseAddress];
+    if (connect_timeout >= 0 && read_timeout >= 0) {
+    	connectTimeout = connect_timeout;
+        readTimeout = read_timeout;
+
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"OK"];
     } else {
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
     }
