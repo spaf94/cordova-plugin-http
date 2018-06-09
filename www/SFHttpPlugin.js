d988690dbf41786214771fbea8573ac84f64f09b
Simão Ferreira <spaf94@outlook.pt>
2018-06-09 14:52:23 +0100

    Added exported methods

--- www/SFHttpPlugin.js
+++ www/SFHttpPlugin.js
@@ -1,5 +1,14 @@
-var exec = require('cordova/exec');
-
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'SFHttpPlugin', 'coolMethod', [arg0]);
+module.exports = {
+    setBaseAddress: function (baseAddress, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "SFHttpPlugin", "setBaseAddress", [baseAddress]);
+    },
+    sendPost: function (endpoint, jsonHeaders, body, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "SFHttpPlugin", "sendPost", [endpoint, jsonHeaders, body]);
+    },
+    sendGet: function (endpoint, jsonHeaders, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "SFHttpPlugin", "sendGet", [endpoint, jsonHeaders]);
+    },
+    setTimeouts: function (connectTimeout, readTimeout, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "SFHttpPlugin", "setTimeouts", [connectTimeout, readTimeout]);
+    }
 };
